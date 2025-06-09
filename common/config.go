4b3f79fe3efed158cc9a75d346f36fcce940fda7
Björn <41452212+bjoern621@users.noreply.github.com>
2025-06-09 23:34:49 +0200

    track received duplicates

--- common/config.go
+++ common/config.go
@@ -6,3 +6,4 @@ const ACK_TIMEOUT_SECONDS = 5
 const RETRIES_PER_PACKET = 2
 const TEAM_ID = 0x2
 const UDP_BUFFER_SIZE_BYTES = 1500 // Standard MTU size for Ethernet
+const RECEIVE_BUFFER_SIZE = 1000   // Number of packets to buffer in the receive channel per peer
