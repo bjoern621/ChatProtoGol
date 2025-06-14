5c7199e15010c73b1b1b2b82ef8d0abfe2298797
Björn <41452212+bjoern621@users.noreply.github.com>
2025-06-14 21:03:36 +0200

    moved dijkstra infrastructure

--- routing/lsdb.go
+++ routing/lsdb.go
@@ -43,6 +43,49 @@ func (r *Router) GetLSA(addr netip.Addr) (LSAEntry, bool) {
 	return LSAEntry{}, false
 }
 
+type DijkstraNode struct {
+	Addr    netip.Addr
+	NextHop *netip.AddrPort
+	Dist    int // Distance from the source node
+	index   int // Index in the priority queue for heap operations
+}
+
+type dijkstraPriorityQueue []*DijkstraNode
+
+func (pq dijkstraPriorityQueue) Len() int { return len(pq) }
+
+func (pq dijkstraPriorityQueue) Less(i, j int) bool {
+	return pq[i].Dist < pq[j].Dist // Min-heap based on distance
+}
+
+func (pq dijkstraPriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *dijkstraPriorityQueue) Push(x any) {
+	node := x.(*DijkstraNode)
+	node.index = len(*pq)
+	*pq = append(*pq, node)
+}
+
+func (pq *dijkstraPriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[:n-1]
+	return item
+}
+
+func (pq *dijkstraPriorityQueue) update(node *DijkstraNode, newDist int, nextHop *netip.AddrPort) {
+	node.Dist = newDist
+	node.NextHop = nextHop
+	heap.Fix(pq, node.index)
+}
+
 // Creates the current topology of the network based on the LSAs in the LSDB.
 // Runs the Dijkstra algorithm to calculate the shortest paths and build the routing table.
 func (r *Router) BuildRoutingTable(socket sock.Socket) {
