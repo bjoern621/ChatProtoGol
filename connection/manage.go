3ddabb54e90ec0e08afc946545784eb049a651a3
Björn <41452212+bjoern621@users.noreply.github.com>
2025-06-18 00:10:53 +0200

    log message

--- connection/manage.go
+++ connection/manage.go
@@ -1,12 +1,17 @@
 package connection
 
-import "net/netip"
+import (
+	"net/netip"
+
+	"bjoernblessin.de/chatprotogol/util/logger"
+)
 
 // ClearUnreachableHosts clears state for hosts that are no longer reachable.
 // This includes removing their LSAs from the LSDB, their sequencing state and their payload buffer in the reconstruction package.
 // May be called with the zero list in which case it does nothing.
 func ClearUnreachableHosts(unreachableHosts []netip.Addr) {
 	for _, addr := range unreachableHosts {
+		logger.Infof("Clearing unreachable host %s", addr)
 		router.RemoveLSA(addr)
 		incomingSequencing.ClearIncomingPacketNumbers(addr)
 		outgoingSequencing.ClearPacketNumbers(addr)
